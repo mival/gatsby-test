eb8d6580ee6c72545a43e9b27ea389da0469f406
mival <jan.vonavka@gmail.com>
2020-10-09 17:37:03 +0200

    seo

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,9 +4,11 @@ export default function Layout({ children }) {
   return (
     <div>
       <Helmet>
-        <meta charSet="utf-8" />
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta name="description" content="A page's description, usually one or two sentences."/>
         <title>My Title</title>
-        <link rel="canonical" href="http://mysite.com/example" />
+        <link rel="canonical" href="https://silly-kalam-44f58e.netlify.app/" />
         <script type="application/ld+json">
           {`
               {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,16 @@
-import Layout from '../components/layout';
-import LikeButton from '../components/LikeButton';
-import { Link } from 'gatsby';
+import Layout from "../components/layout"
+import LikeButton from "../components/LikeButton"
+import { Link } from "gatsby"
 import React from "react"
 
 export default function Home() {
-  return <div><Layout>Hello world!<LikeButton/><Link to='/about'>About</Link></Layout></div>
+  return (
+    <div>
+      <Layout>
+        <h1>Hello world!</h1>
+        <LikeButton />
+        <Link to="/about">About</Link>
+      </Layout>
+    </div>
+  )
 }
