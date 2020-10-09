0439c5153f24e0b49e1f3113e1d85a379d1793d6
mival <jan.vonavka@gmail.com>
2020-10-09 17:51:10 +0200

    maskable icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,19 +9,60 @@ module.exports = {
   plugins: ['gatsby-plugin-sass', 'gatsby-plugin-react-helmet', {
     resolve: 'gatsby-plugin-manifest',
     options: {
-      name: "GatsbyJS",
-      short_name: "GatsbyJS",
-      start_url: "/",
-      background_color: "#6b37bf",
-      theme_color: "#6b37bf",
-      // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+      name: 'GatsbyJS',
+      short_name: 'GatsbyJS',
+      start_url: '/',
+      background_color: '#6b37bf',
+      theme_color: '#6b37bf',
+      // Enables 'Add to Homescreen' prompt and disables browser UI (including back button)
       // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-      display: "standalone",
-      icon: "src/assets/images/icon.png", // This path is relative to the root of the site.
+      display: 'standalone',
+      icon: 'src/assets/images/icon.png', // This path is relative to the root of the site.
+      icons:[
+        {
+          src: "icons/icon.png",
+          sizes: "48x48",
+          type: "image/png"
+        },{
+          src: "icons/icon-72x72.png",
+          sizes: "72x72",
+          type: "image/png"
+        },{
+          src: "icons/icon-96x96.png",
+          sizes: "96x96",
+          type: "image/png"
+        },{
+          src: "icons/icon-144x144.png",
+          sizes: "144x144",
+          type: "image/png"
+        },{
+          src: "icons/icon-192x192.png",
+          sizes: "192x192",
+          type: "image/png"
+        },{
+          src: "icons/icon-256x256.png",
+          sizes: "256x256",
+          type: "image/png"
+        },{
+          src: "icons/icon-384x384.png",
+          sizes: "384x384",
+          type: "image/png"
+        },{
+          src: "icons/icon-512x512.png",
+          sizes: "512x512",
+          type: "image/png"
+        },
+        {
+          src: '/maskable_icon.png',
+          sizes: '480x480',
+          type: 'image/png',
+          purpose: 'maskable'
+        }
+      ],
       // An optional attribute which provides support for CORS check.
       // If you do not provide a crossOrigin option, it will skip CORS for manifest.
-      // Any invalid keyword or empty string defaults to `anonymous`
-      crossOrigin: `use-credentials`,
+      // Any invalid keyword or empty string defaults to 'anonymous'
+      crossOrigin: 'use-credentials',
     },
   }, 'gatsby-plugin-offline'],
 }
