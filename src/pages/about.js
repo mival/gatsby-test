4e70e0112f8692569613191ddaae0d368c2c5388
mival <jan.vonavka@gmail.com>
2020-10-09 17:11:40 +0200

    sass, image

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import FiestaImg from '../assets/images/fiesta.jpg'
+import React from 'react'
+const AboutPage = () => (
+  <main>
+    <h1>About the Author</h1>
+    <p>Welcome to my Gatsby site.</p>
+    <img src={FiestaImg} alt="A dog smiling in a party hat" />
+  </main>
+)
+export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
+import Layout from '../components/layout';
 import LikeButton from '../components/LikeButton';
+import { Link } from 'gatsby';
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!<LikeButton/></div>
+  return <div><Layout>Hello world!<LikeButton/><Link to='/about'>About</Link></Layout></div>
 }
